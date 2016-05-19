b140824c702ff482627221dc26c72b918f77f328
Phan Thanh Hai <thanhhai.red@gmail.com>
2016-05-19 11:40:26 +0700

    1
    1

--- travel/miami/js/main.js
+++ travel/miami/js/main.js
@@ -1,20 +1,21 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
- 
-// -----------------------------
-// Smooth Scroll
-// -----------------------------
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-$(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
     });
+  });
 });
