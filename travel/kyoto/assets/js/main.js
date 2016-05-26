9884e3e4ceeaedc6d1cb4c92c9f4603317cca08a
Phan Thanh Hai <thanhhai.red@gmail.com>
2016-05-26 17:42:30 +0700

    1
    1

--- travel/kyoto/assets/js/main.js
+++ travel/kyoto/assets/js/main.js
@@ -16,3 +16,29 @@ $(function() {
         }
     });
 });
+
+
+
+// -----------------------------
+// Tooltip
+// -----------------------------
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+$('#toggle_fullscreen').click(function() {
+    screenfull.toggle();
+});
+
+
+! function(t) { "use strict";
+    var a = t("[data-slides]"),
+        ni = 0,
+        s = a.data("slides"),
+        e = s.length,
+        n = function() {
+            if (ni >= a.length) { ni = 0 } else { ni++ };
+            a.css("background-image", 'url("' + s[ni] + '")').show(0, function() { setTimeout(n, 5e3) }) };
+    n() }(jQuery);
