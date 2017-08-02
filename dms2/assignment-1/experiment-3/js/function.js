6dbab67920927a0cf34920100394a58b79d13844
thanhhaired <thanhhai.red@gmail.com>
2017-08-03 06:55:31 +1000

    1
    1

--- dms2/assignment-1/experiment-3/js/function.js
+++ dms2/assignment-1/experiment-3/js/function.js
@@ -1,5 +1,6 @@
 var tone = document.getElementById("tabone");
 var ttwo = document.getElementById("tabtwo");
+var tthree = document.getElementById("tabthree");
 var iframe = document.getElementById("if");
 var urlbar = document.getElementById("urlbar");
 
@@ -7,6 +8,7 @@ function tabone() {
     if (tone.className.match("tab selected")) {} else {
         tone.className = "tab selected";
         ttwo.className = "tab";
+        tthree.className = "tab";
         iframe.src = "http://interactivedesignjournal.thanhhai.red/";
         urlbar.innerHTML = "interactivedesignjournal.thanhhai.red";
     }
@@ -16,8 +18,19 @@ function tabtwo() {
     if (ttwo.className.match("tab selected")) {} else {
         ttwo.className = "tab selected";
         tone.className = "tab";
+        tthree.className = "tab";
         iframe.src = "http://thanhhai.red/";
         urlbar.innerHTML = "thanhhai.red";
+    }
+}
+
+function tabthree() {
+    if (tthree.className.match("tab selected")) {} else {
+        tthree.className = "tab selected";
+        tone.className = "tab";
+        ttwo.className = "tab";
+        iframe.src = "../experiment-1";
+        urlbar.innerHTML = "experiment-1";
     }
 }
 
